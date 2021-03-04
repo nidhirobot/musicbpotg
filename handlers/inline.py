6b8ad4d699dc980ba962c44e011a41fa8deb2afa
Aju <73631177+nidhirobot@users.noreply.github.com>
2021-03-04 16:13:08 +0530

    Update inline.py

--- handlers/inline.py
+++ handlers/inline.py
@@ -16,7 +16,7 @@ async def search(client: Client, query: InlineQuery):
         await client.answer_inline_query(
             query.id,
             results=answers,
-            switch_pm_text="Type a YouTube video name...",
+            switch_pm_text="Bsdk gaane ka naam likhega bhi?",
             switch_pm_parameter="help",
             cache_time=0
         )
@@ -49,6 +49,6 @@ async def search(client: Client, query: InlineQuery):
             await query.answer(
                 results=answers,
                 cache_time=0,
-                switch_pm_text="Error: Search timed out",
+                switch_pm_text="Gand maar li tune vro...",
                 switch_pm_parameter="",
             )
