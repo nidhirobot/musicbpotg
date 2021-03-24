d606457aea688cdfd1c80984d27a042005cac7ab
Aju <73631177+nidhirobot@users.noreply.github.com>
2021-03-24 16:14:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-SESSION_NAME = getenv("SESSION_NAME", "session")
+SESSION_NAME = AQCjdlJpzO-J30NEJR9KtcIQiQYYcGA0ZEXwkQGDjEJX4kQMQUzeWfWbqAy2Cisw_TZPvkRRWVfRddZriJJlV-pGJvc9VIhqSLaDbyXGCgl9zH_q9xAgizVqcjJ79wBRd6fe9CtlEBi8GaPZn1hBuJFsR27qMm7ySKXjX1r-e_BkHQonMSDBCxWveix1ccm9imEa6kjItIEWirUW6EeaKclHQ50bkvd_irxYdc4qPgVTeE9nlZA4SWc9q2eJ58CpBoKMKqmGEOrfMad6dYzNiqCeWrCHKSfRpbGbtwUs_Trv1o3JgtSwTrhZrDA7RFiQNcVB1jPVqsW29HMFjH5dqlTFPH0wA
 BOT_TOKEN = getenv("BOT_TOKEN")
 
 API_ID = int(getenv("API_ID"))
